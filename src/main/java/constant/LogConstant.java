1e175ebc94898474b4e531118564bce7aefd77be
JillW <897615138@qq.com>
2020-11-04 11:05:38 +0800

    initial commit
    Signed-off-by: JillW <897615138@qq.com>

---
+++ src/main/java/constant/LogConstant.java
@@ -0,0 +1,13 @@
+package constant;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author JillW
+ * @date 2020/09/30
+ */
+public class LogConstant {
+    public static final Logger log = LoggerFactory.getLogger(LogConstant.class);
+    public static final String CROSS_LINE = "---------------------------";
+}
